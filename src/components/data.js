9e411173c83c0b4b772103569ae4a0a5f60f798c
developerjay18 <jayrai3003@gmail.com>
2023-11-03 18:56:29 +0530

    adding components

--- src/components/data.js
+++ src/components/data.js
@@ -1,3 +1,5 @@
+import { FBIcon, WPIcon, YoutubeIcon, InstaIcon } from '../assets';
+
 const navItems = [
   {
     name: 'Home',
@@ -17,4 +19,43 @@ const navItems = [
   },
 ];
 
-export { navItems };
+const socialLinks = [
+  {
+    imageURL: FBIcon,
+    slug: 'https://www.facebook.com/decentsettlement',
+  },
+  {
+    imageURL: InstaIcon,
+    slug: 'https://www.instagram.com/decentsettlement/',
+  },
+  {
+    imageURL: YoutubeIcon,
+    slug: 'https://www.youtube.com/@DecentSettlement',
+  },
+  {
+    imageURL: WPIcon,
+    slug: 'https://wa.me/919824336161',
+  },
+];
+
+const services = [
+  'Personal Loan Settlement',
+  'Business Loan Settlement',
+  'Credit Card Settlement',
+  'Anti-Harassment Service',
+  'All Types of Loan Closure',
+  'Credit Score Booster',
+];
+
+const resources = [
+  'Personal Loan',
+  'Business Loan',
+  'Credit Card',
+  'C.C. and O.D. Loan',
+  'Gold Loan',
+  'Bad Credit CIBIL Score Loan',
+  'Banking Ombudsman',
+  'RBI Complaints Against Banks',
+];
+
+export { navItems, socialLinks, services, resources };
--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import Container from './Container/Container';
 import Button from './Button';
 import ScaleDiv from './ScaleDiv';
 import BurgerMenu from './BurgerMenu';
+import Logo from './Logo';
 
-export { Header, Footer, Container, Button, ScaleDiv, BurgerMenu };
+export { Header, Footer, Container, Button, ScaleDiv, BurgerMenu, Logo };
