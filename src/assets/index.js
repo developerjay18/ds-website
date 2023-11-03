888a36aafc952cedc420939a2b2eb835b41d75ee
developerjay18 <jayrai3003@gmail.com>
2023-11-03 18:54:18 +0530

    adding images and routes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,7 @@
-import Logo from "./logo.png"
+import Logo from './logo.png';
+import WPIcon from './whatsapp.svg';
+import InstaIcon from './instagram.svg';
+import YoutubeIcon from './youtube.svg';
+import FBIcon from './facebook.svg';
 
-export {Logo}
+export { Logo, WPIcon, InstaIcon, YoutubeIcon, FBIcon };
