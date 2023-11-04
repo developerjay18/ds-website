5b722cace625c9556a2b196f69ca3d38de979d3f
developerjay18 <jayrai3003@gmail.com>
2023-11-04 08:52:54 +0530

    adding images

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,7 @@ import WPIcon from './whatsapp.svg';
 import InstaIcon from './instagram.svg';
 import YoutubeIcon from './youtube.svg';
 import FBIcon from './facebook.svg';
+import Gmail from './gmail.svg';
+import PCall from './pcall.png';
 
-export { Logo, WPIcon, InstaIcon, YoutubeIcon, FBIcon };
+export { Logo, WPIcon, InstaIcon, YoutubeIcon, FBIcon, Gmail, PCall };
