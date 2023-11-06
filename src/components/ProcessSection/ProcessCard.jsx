0878e3319344d9b210a987733655552553d3e6db
developerjay18 <jayrai3003@gmail.com>
2023-11-06 23:05:38 +0530

    updating process section

--- src/components/ProcessSection/ProcessCard.jsx
+++ src/components/ProcessSection/ProcessCard.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 function ProcessCard({ imgSrc, title, detail }) {
   return (
-    <div className="shadow-lg hover:shadow-profBlue cursor-pointer py-5 px-3 rounded-lg w-[31%] flex flex-col justify-center items-center gap-5 bg-[#333333] text-white">
+    <div className="shadow-lg hover:shadow-profBlue cursor-pointer py-5 px-3 rounded-xl w-[31%] flex flex-col justify-center items-center gap-5 bg-[#eeeeee] text-mateBlack">border
       {/* images */}
       <div className="image flex justify-center h-[40%]">
         <img
           src={imgSrc}
           alt="process-img"
-          className="bg-white border-2 border-profBlue cloudy-wavy p-2 w-[60%] hover:shadow-lg hover:shadow-profBlue"
+          className="bg-white cloudy-wavy p-2 w-[60%] hover:shadow-lg hover:shadow-profBlue"
         />
       </div>
-      <div className="title h-[10%] w-full flex justify-center items-center font-semibold capitalize text-2xl hover:text-white text-profBlue">
+      <div className="title h-[10%] w-full flex justify-center items-center font-semibold capitalize text-2xl hover:text-profBlue text-mateBlack">
         {title}
       </div>
-      <div className="detail h-[50%] text-lightGrey text-center text-md">
+      <div className="detail h-[50%] text-mateBlack text-md text-center text-md">
         {detail}
       </div>
     </div>
