6a6c0b6ac9ff8271b92cf5393067a05d6790f681
developerjay18 <jayrai3003@gmail.com>
2023-11-06 21:26:38 +0530

    adding images

---
+++ src/assets/services/index.js
@@ -0,0 +1,6 @@
+import AntiHarassment from './anti-harrasement.jpg';
+import CardSettlement from './card-settlement.jpg';
+import DebtClosure from './debt-closure.jpg';
+import PersonalLoan from './personal-loan.jpg';
+
+export { AntiHarassment, CardSettlement, DebtClosure, PersonalLoan };
