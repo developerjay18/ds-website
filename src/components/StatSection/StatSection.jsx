0886e5bea780890fc52aa75fdbc35fb5f44c2999
developerjay18 <jayrai3003@gmail.com>
2023-11-06 23:07:48 +0530

    adding stats

---
+++ src/components/StatSection/StatSection.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import CountUp from 'react-countup';
+
+function StatSection() {
+  const controls = useAnimation();
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 500) {
+        setIsVisible(true);
+        controls.start({ opacity: 1, scale: 1 });
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [controls]);
+
+  const statsData = [
+    {
+      number: '5500',
+      extension: '+',
+      title: 'client conversations',
+    },
+    {
+      number: '10',
+      extension: 'CR+',
+      title: 'debt handled',
+    },
+    {
+      number: '700',
+      extension: '+',
+      title: 'trusted clinets',
+    },
+    {
+      number: '100',
+      extension: '%',
+      title: 'years of experience',
+    },
+  ];
+
+  return (
+    <div className="font-Poppins w-full flex px-5 lg:px-24 py-16 justify-between flex-col md:flex-row flex-wrap gap-12 md:gap-0 lg:gap-0 bg-[#f4f4f4]">
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={controls}
+        transition={{ duration: 1 }}
+        className="w-full flex justify-between flex-col md:flex-row gap-12 md:gap-0"
+      >
+        {statsData.map((data) => (
+          <div
+            id="count-up-section"
+            className="w-full md:w-[50%] lg:w-[17%]"
+            key={data.number}
+          >
+            {isVisible && (
+              <div className="flex flex-col justify-center items-center gap-3 md:py-5 lg:py-0">
+                <div className=" text-4xl lg:text-5xl text-gradient font-semibold ">
+                  <span>
+                    <CountUp
+                      start={0}
+                      end={data.number}
+                      duration={7}
+                      className=""
+                    />
+                  </span>
+                  <span>{data.extension}</span>
+                </div>
+                <div className="text-md md:text-lg capitalize">
+                  {data.title}
+                </div>
+              </div>
+            )}
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  );
+}
+
+export default StatSection;
