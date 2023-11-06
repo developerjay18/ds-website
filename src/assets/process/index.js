c32dc0ca23272a6cede5c03dae3e086d4f5521b8
developerjay18 <jayrai3003@gmail.com>
2023-11-06 18:15:07 +0530

    adding images

---
+++ src/assets/process/index.js
@@ -0,0 +1,7 @@
+import Settle from './Discussion-rafiki.svg';
+import Eligibility from './team checklist-cuate.svg';
+import Select from './Select-pana.svg';
+import Savings from './Money income-amico.svg';
+import Negotiate from './International trade-bro.svg';
+
+export { Settle, Eligibility, Select, Savings, Negotiate };
