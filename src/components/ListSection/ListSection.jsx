06fa695e8fa9e1cd030391b2ef5c870001d9f7f0
developerjay18 <jayrai3003@gmail.com>
2023-11-09 10:55:41 +0530

    adding lists

--- src/components/ListSection/ListSection.jsx
+++ src/components/ListSection/ListSection.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { services, resources } from './data';
-import ScaleDiv from '../ScaleDiv';
-import { ListImage } from '../../assets';
+import { ScaleDiv, Button } from '../index';
+import { Logo } from '../../assets';
 
 function ListSection() {
   const [hover, setHover] = useState(false);
@@ -9,7 +9,7 @@ function ListSection() {
   return (
     <div className="px-5 lg:px-24 py-16 font-Poppins flex justify-between">
       {/* servies */}
-      <div className="services bg-[#eeeeee] rounded-lg p-5 flex flex-col gap-6">
+      <div className="shadow-lg services bg-[#eeeeee] rounded-lg p-5 flex flex-col gap-6">
         {/* heading section */}
         <div className="flex flex-col gap-8 text-white">
           <div
@@ -47,15 +47,15 @@ function ListSection() {
         </ul>
       </div>
       {/* image */}
-      <div className="image w-[40%]">
+      {/* <div className="image w-[40%]">
         <img
           src={ListImage}
           alt="list-image"
           className="h-full object-cover rounded-lg"
         />
-      </div>
+      </div> */}
       {/* resources */}
-      <div className="resources bg-[#eeeeee] rounded-lg p-5 flex flex-col gap-6">
+      <div className="shadow-lg resources bg-[#eeeeee] rounded-lg p-5 flex flex-col gap-6">
         {/* heading section */}
         <div className="flex flex-col gap-8 text-white">
           <div
@@ -91,6 +91,21 @@ function ListSection() {
             </ScaleDiv>
           ))}
         </ul>
+      </div>
+      {/* extra */}
+      <div className="shadow-lg hover:shadow-profBlue w-[50%] bg-mateBlack px-5 py-4 rounded-lg text-lightGrey flex flex-col gap-5 cursor-pointer">
+        <div className="w-[50%]">
+          <img src={Logo} alt="logo" className="" />
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio maiores
+          ratione deserunt, voluptas beatae, cupiditate aliquid amet
+          reprehenderit architecto, saepe tenetur minima mollitia qui incidunt
+          tempore recusandae ipsam totam iusto.
+        </p>
+        <div className="pt-5">
+          <Button className={`text-white`}>Book an appointment</Button>
+        </div>
       </div>
     </div>
   );
