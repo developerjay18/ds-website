eb8c3098c82a535092e844f55f7d23160ed37365
developerjay18 <jayrai3003@gmail.com>
2023-12-19 16:50:42 +0530

    delivering website after completing final review bugs solution

--- src/assets/reviews/index.js
+++ src/assets/reviews/index.js
@@ -1,3 +1,10 @@
-import Yash from './reviewer.jpeg';
+import one from './1.jpeg';
+import zero from './0.jpg';
+import three from './3.png';
+import four from './4.png';
+import five from './5.png';
+import six from './6.png';
+import seven from './7.jpeg';
+import eight from './8.jpeg';
 
-export { Yash };
+export { one, zero, three, four, five, six, seven, eight };
--- src/components/SliderSection/data.js
+++ src/components/SliderSection/data.js
@@ -1,44 +1,44 @@
-import { Yash } from '../../assets/reviews';
+import * as Img from '../../assets/reviews';
 
 const data = [
   {
-    imageUrl: Yash,
+    imageUrl: Img.one,
     location: 'surat',
     review:
       'I am truly grateful to Decent Settlement for helping me settle my personal loans. I never thought it was possible! In less than 6 months, I am now debt-free. I am thankful for a decent settlement.',
   },
   {
-    imageUrl: Yash,
+    imageUrl: Img.zero,
     location: 'ahmedabad',
     review: `I'm really grateful to Decent Settlement for helping me clear my personal loans. It seemed impossible, but in less than 10 months, I'm now debt-free. Thanks, Decent Settlement!`,
   },
   {
-    imageUrl: Yash,
+    imageUrl: Img.three,
     location: 'gandhinagar',
     review: `I was constantly harassed by recovery agents for 5 months, and then Decent Settlement came to my rescue! Their program is a lifesaver for heavy debt, and I'm thankful they prevented my bankruptcy`,
   },
   {
-    imageUrl: Yash,
+    imageUrl: Img.four,
     location: 'rajkot',
     review: `Decent Settlement saved me from a money mess. Their team was like a guide, helping me step by step. Now, I don't owe anyone money, all thanks to them being super awesome!`,
   },
   {
-    imageUrl: Yash,
+    imageUrl: Img.five,
     location: 'vadodara',
     review: `Business loans were making me feel stuck, but Decent Settlement helped me get unstuck. They talked to the money people and got me a great deal. Really thankful for their quick and smart help!`,
   },
   {
-    imageUrl: Yash,
+    imageUrl: Img.six,
     location: 'mehsana',
     review: `Credit cards were confusing, but Decent Settlement helped me out! They talked to the money wizards and got my bills down. Now I'm on my way to a better money life, thanks to these cool folks!`,
   },
   {
-    imageUrl: Yash,
+    imageUrl: Img.seven,
     location: 'surendranagar',
     review: `Closing loans made easy! Decent Settlement took care of everything. No headaches, no hassles. Super happy with their smooth and easy service!`,
   },
   {
-    imageUrl: Yash,
+    imageUrl: Img.eight,
     location: 'jamnagar',
     review: `Had a problem with the bank? Decent Settlement filed a complaint for me! They know all the rules and fought for me. Big thanks for standing up for us regular folks!`,
   },
